8b60dec9a1e5a0db6f153c1990ca927e4f69c7a9
iam-sourabh <i.am.sourabh3@gmail.com>
2021-02-20 11:12:05 +0530

    sorting list based on first name

--- src/app/app.service.service.ts
+++ src/app/app.service.service.ts
@@ -1,46 +1,51 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { NgxSpinnerService } from "ngx-spinner";
-import { BehaviorSubject } from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {NgxSpinnerService} from "ngx-spinner";
+import {BehaviorSubject} from "rxjs";
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({providedIn: "root"})
 export class AppService {
-  url = "https://reqres.in/api/users";
-  userList = new BehaviorSubject(null);
-  userInfo = new BehaviorSubject(null);
-  pageNos = new BehaviorSubject(null);
-  handelPages = new BehaviorSubject(false);
+    url = "https://reqres.in/api/users";
+    userList = new BehaviorSubject(null);
+    userInfo = new BehaviorSubject(null);
+    pageNos = new BehaviorSubject(null);
+    handelPages = new BehaviorSubject(false);
 
-  constructor(private spinner: NgxSpinnerService, private http: HttpClient) {}
+    constructor(private spinner : NgxSpinnerService, private http : HttpClient) {}
 
-  getUserList(pageNo) {
-    return this.http.get<any>(this.url + "?page=" + pageNo);
-  }
+    getUserList(pageNo) {
+        return this.http.get<any>(this.url + "?page=" + pageNo);
+    }
 
-  getUserById(id){
-    return this.http.get<any>(this.url + "/" + id);
-  }
+    getUserById(id) {
+        return this.http.get<any>(this.url + "/" + id);
+    }
 
-  loadUserList(pageNo) {
-    this.spinner.show();
-    this.getUserList(pageNo).subscribe((data) => {
-      this.userList.next(data.data);
-      this.spinner.hide();
-    });
-  }
+    loadUserList(pageNo) {
+        let list;
+        this.spinner.show();
+        this.getUserList(pageNo).subscribe((data) => {
+            list = data.data;
+            list.sort((a, b) => {
+                var textA = a.first_name.toUpperCase();
+                var textB = b.first_name.toUpperCase();
+                return(textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+            })
+            this.userList.next(list);
+            this.spinner.hide();
+        });
+    }
 
-  loadUserInfo(id) {
-    this.spinner.show();
-    this.getUserById(id).subscribe((data) => {
-      this.userInfo.next(data.data);
-      this.spinner.hide();
-    });
-  }
+    loadUserInfo(id) {
+        this.spinner.show();
+        this.getUserById(id).subscribe((data) => {
+            this.userInfo.next(data.data);
+            this.spinner.hide();
+        });
+    }
 
 
-  showInfoPage(v) {
-    this.handelPages.next(v);
-  }
+    showInfoPage(v) {
+        this.handelPages.next(v);
+    }
 }
